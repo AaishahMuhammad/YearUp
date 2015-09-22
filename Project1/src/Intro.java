491c4cd56d3a78a10166086816e22cede5c84a50
AaishahMuhammad <Amuhammad@atl.yearup.org>
2015-09-22 14:34:31 -0400

    yaay

---
+++ Project1/src/Intro.java
@@ -0,0 +1,13 @@
+
+public class Intro {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Aaishah");
+		System.out.println("My favorite movie is The Goonies");
+		System.out.println("I went on a 7 day cruise to Belize, Honduras, and Mexico");
+		
+		
+
+	}
+
+}
