73005029b505cdc3393aaa6fc1cdcb7ee2289b03
AaishahMuhammad <Amuhammad@atl.yearup.org>
2015-10-23 10:54:25 -0400

    Largest Number

--- Project11/src/LargestNumber.java
+++ Project11/src/LargestNumber.java
@@ -1,21 +1,19 @@
 
 public class LargestNumber {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
+		int[] num = {-1, 25, 33, 97, -4, -5, 87, 2334, 88675, 0-24};
+		int[] nums = {32, 443, 414, -14, 90, -1444, 94049, 985, 24, 2784};
+		LargestNumberUsingWhileLoop(num);	
+		LargestNumberUsingForLoop(nums);
+		}
 		
-	}
-	public static void FindLargestNumberUsingWhileLoop(int[] numbers)
-	{
-		if(numbers != null)
-		{
-			if (numbers.length > 0)
-			{
+	public static void LargestNumberUsingWhileLoop(int[] numbers){
+		if(numbers != null){
+			if (numbers.length > 0){
 			int i = 1;
 			int largestNumber = numbers[0];
-			
-			while (i < numbers.length)
-			{
+			while (i < numbers.length){
 				if (largestNumber < numbers[i])
 				{
 			       largestNumber = numbers[i];
@@ -24,7 +22,7 @@ public class LargestNumber {
 			i++;
 	        }}}}
 		
-			public static void largestNumberUsingForLoops(int[]numbers){
+			public static void LargestNumberUsingForLoop(int[]numbers){
 				if (numbers != null){
 					if(numbers.length > 0){
 						int i;
@@ -35,11 +33,11 @@ public class LargestNumber {
 								largestnumber = numbers[i];
 							}
 						}
-						
+						System.out.println("Largest number using for loop is " + largestnumber);
 					}
 				}
 			}
-		  }	
+		 }	
 		
 	
 
