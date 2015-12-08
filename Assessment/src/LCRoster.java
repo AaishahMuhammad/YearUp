6760ada68ba285765628f8496e0480fedfb0afd2
AaishahMuhammad <Amuhammad@atl.yearup.org>
2015-12-08 11:20:56 -0500

    Assessment

---
+++ Assessment/src/LCRoster.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class LCRoster {
+	private static final String LcId = null;
+
+	public static void main(String[]args0){
+		
+	}
+	
+	private String students;
+	private String lcName;
+	private int lcId;
+	
+	LCRoster(String students, String lcName, int lcId){
+		this.students = students;
+		this.lcName = lcName;
+		this.lcId = lcId;
+	}
+	
+	public String getStudents() {
+		return students;
+	}
+	public String getLcName() {
+		return lcName;
+	}
+	public String setlcName(){
+		return lcName;
+	}
+	public int getLcId() {
+		return lcId;
+	}
+	public String setLcId(){
+		return LcId;
+	}
+	
+		
+	}
+	
+	
+
+
